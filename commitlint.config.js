cd0ad1360f5d712ffa370d9d35b019fabbf34ef6
Zameer.Mohamed <zameer.mohamed@dvsa.gov.uk>
2025-06-10 10:40:28 +0100

    adding commitlint.config for convention commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,19 +1,5 @@
-import type { UserConfig } from "@commitlint/types";
-import * as fs from "fs";
-
-const Configuration: UserConfig = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "header-max-length": [2, "always", 200],
-    "scope-enum": [
-      2,
-      "always",
-      [
-        ...fs.readdirSync("app").filter((file) => fs.statSync(`app/${file}`).isDirectory()),
-        ...fs.readdirSync("infra").filter((file) => fs.statSync(`infra/${file}`).isDirectory()),
-      ],
-    ],
-  },
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
 };
 
-export default Configuration;
+
